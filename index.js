5fe0348d2b27c7741357b7a91d775c1af6b57564
dungnnh <dung.nnh5930@sinhvien.hoasen.edu.vn>
2023-02-12 22:35:57 +0700

    chinh sua dieu kien 2

---
+++ index.js
@@ -0,0 +1,27 @@
+let a = "ATCGTAC";
+console.log(a);
+let b = "TAGTCAC";
+console.log(b);
+ function Check(a,b) {
+    let count = 0;
+
+    for (let i = 0; i < a.length ; i++)
+    {
+        let temp = i;
+        a==i;
+        for(let j = 0; j < b.length ; j++)
+        {
+            if (n[temp] === a[j])
+            {
+                count++;
+                temp++;
+            } else {i++}
+    }
+
+ }
+ return count;
+}
+
+
+
+ console.log(Virus(n,m));
